cd850353dacd54f33642883a410ab206281b7279
Alice Deveops <hlmaab@connect.ust.hk>
2018-06-18 18:50:50 +0800

    database error

--- application/controllers/Insert.php
+++ application/controllers/Insert.php
@@ -14,7 +14,7 @@ class Insert extends BaseController{
       $this->isLoggedIn();   
   }
 
-  /*function index(){
+  function index(){
     //load libraries
     $this->load->helper(array('form', 'url'));
     $this->load->library('form_validation');
@@ -50,46 +50,7 @@ class Insert extends BaseController{
     }
 
     log_message('info', '(insert.php) The purpose of this function is to insert data.');
-  }*/
-function index(){
-    //load libraries
-    $this->load->helper(array('form', 'url'));
-    $this->load->library('form_validation');
-
-
-
-    //set rules
-    $this->form_validation->set_rules('sname', 'sname', 'required');
-
-    // get field data from addSoftware.php
-    $data = array ('new_data' => $this->input->post('new_data'));
-
-    //$data = array ('new_data' => $this->input->post('test'));
-    print_r($data);
-
-
-    //check if the validation is successful
-    if ($this->form_validation->run() == FALSE)
-    {
-      //Loading the from view
-      $this->global['pageTitle'] = 'CodeInsect : Add Software';
-      $this->loadViews("addSoftware/addSoftware", $this->global, NULl , NULL);
-
-      log_message('error', '(insert.php) Name field is missing in the form.');
-    }
-    else
-    {
-      $this->insert_model->form_insert($data);
-
-      //Loading the successfully inserted view
-      $this->global['pageTitle'] = 'CodeInsect : Added Software';
-      $this->loadViews('addSoftware/addSoftwareSuccess', $this->global, NULl , NULL);
-
-      log_message('debug', '(insert.php) Name field is valid in the form.');
-    }
-
-    log_message('info', '(insert.php) The purpose of this function is to insert data.');
-  }
+  }  
 }
 
 ?>
